f41ca87990b92486f8995dad39d2d1943622a669
Ht <htcvhs971@gmail.com>
2023-02-08 15:13:09 +0100

    Declare form in main.js without validations

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+// On part de la fonction issue de BOOTSTRAP
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+//=============== START - DECLARATION DU FORMULAIRE ===============//
+(function () {
+	'use strict';
+	// 1- Déclaration form
+	let form = document.getElementById('lessonForm');
+
+	// 2- addEventListener au submit qui lance fonction ac 1 event
+	form.addEventListener(
+		'submit',
+		function (event) {
+			//3- On mets tous les elts du form ds 1 tbl : Array.from(form.elements)
+			Array.from(form.elements).forEach((input) => {
+				//4- Chq type de input diff de submit
+				if (input.type !== 'submit') {
+					//5- On rentre tous les input ds tbl et les valident 1par1
+					if (!validateFields(input)) {
+						//Ajout lors des Validations
+						alert('1');
+						//6- Stopper absolument la validation
+						event.preventDefault();
+						//7- Stopper absolument les propagations
+						event.stopPropagation();
+
+						//8- Stylisation input (avec Bootstrap)
+						//8-a. Add&Remove class
+						input.classList.remove('is-valid');
+						input.classList.add('is-invalid');
+						//8-b. Search elt à coté & donne un style
+						input.nextElementSibling.style.display = 'block';
+					} else {
+						//Ajout lors des Validations
+						alert('3');
+						//9-écriture de l'inv à 8-
+						input.nextElementSibling.style.display = 'none';
+						input.classList.remove('is-invalid');
+						input.classList.add('is-valid');
+					}
+				}
+			});
+		},
+		false
+	);
+})();
+//=============== END - DECLARATION DU FORMULAIRE ===============//
